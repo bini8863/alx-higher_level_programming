f4923170ace02736d0228a5a7149397bca8af5cf
biniyam <bengaminfish@gmail.com>
2022-12-01 10:53:40 -0800

    task 4

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    def maths():
+        def add(a, b):
+            return a + b
+        def sub(a, b):
+            return a - b
+        def mul(a, b):
+            return a * b
+        def div(a, b):
+            return a / b
