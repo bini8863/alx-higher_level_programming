5e157c72ddb435c86e7abedb6acf19a7a321dd45
biniyam <bengaminfish@gmail.com>
2022-12-01 18:43:55 -0800

    removed unneccessary emptylines from 1-calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-
     """prints the sum, difference, quotient, and product of 10 and 5"""
-
     from calculator_1 import add, sub, mul, div
 
     a = 10
