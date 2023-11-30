22e902d4968491d6667f2bffb04a39baf4f70bcc
Biniyam Fisseha <bengaminfish@gmail.com>
2023-11-30 12:49:31 +0000

    pycodestyle compatable

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
 
-import sys
+from calculator_1 import add, sub, mul, div
 
-length = len(sys.argv)
-summation = 0
+if __name__ == "__main__":
+    a = 10
+    b = 5
 
-
-if __name__ == '__main__':
-	if length == 1:
-		summation = 0
-	else:
-		for i in range(1, length):
-			
-			summation += int(sys.argv[i])
-		print("{:d}".format(summation))
-	
-	
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
