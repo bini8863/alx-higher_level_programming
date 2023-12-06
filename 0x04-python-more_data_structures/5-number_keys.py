7ad98c1aeb5b5864036a89f8058937c4f9c6932b
Biniyam Fisseha <bengaminfish@gmail.com>
2023-12-06 15:30:57 +0000

    shibang line error

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,7 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
 
-# number_keys(a_dictionary): returns the number of keys in a dicionary
+# number_keys(a_dictionary) - returns the number of keys in a dicionary
 
 def number_keys(a_dictionary):
     return(len(a_dictionary.keys())
+
