426f9ceb2448ce5dea805e892f7465bf0f6eec17
Biniyam Fisseha <bengaminfish@gmail.com>
2023-12-06 16:50:44 +0000

    2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    new = list(set(my_list))
+    list_length = len(new)
+    summation = 0
+    for i in range(list_length):
+        summation = summation+new[i]
+    print(summation)
