25a877cbc4a398949dfd07a3e55df6a4d05bac73
Biniyam Fisseha <bengaminfish@gmail.com>
2023-12-06 16:26:49 +0000

    used a slicker map function

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    squared = []
-    for x in range(len(matrix)):
-        squared.append(x**2)
+    squared = list(map(lambda x: x**2, range(matrix)))
     return(squared)
